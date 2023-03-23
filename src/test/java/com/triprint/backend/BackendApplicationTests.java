597dfa31ac807892db3a22a2772c7670975bd212
dana <seonhui9759@naver.com>
2023-03-23 21:02:51 +0900

    fix : 에러 수정
    - test코드 안에 내용이 없어서 에러가 남

--- src/test/java/com/triprint/backend/BackendApplicationTests.java
+++ src/test/java/com/triprint/backend/BackendApplicationTests.java
@@ -1,13 +1,7 @@
 package com.triprint.backend;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BackendApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
