e560cf584d955e87f4e32d490a8f58bd954120fd
dana <seonhui9759@naver.com>
2023-03-27 21:40:57 +0900

    chore(CI/CD) : Docker에서 build 및 실행시 필요한 설정 파일들 추가

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
 FROM openjdk:11
 ARG JAR_FILE=build/libs/*.jar
+ARG SCRIPT_FILE=scripts/run.sh
 COPY ${JAR_FILE} app.jar
-ENTRYPOINT ["java","-jar","/app.jar"]
+COPY ${SCRIPT_FILE} run.sh
+ENTRYPOINT ["sh", "run.sh"]
---
+++ scripts/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+java -jar ./app.jar \
+  --spring.datasource.url="$DB_URL" \
+  --spring.datasource.username="$DB_NAME" \
+  --spring.datasource.password="$DB_PASSWORD" \
+  --spring.security.oauth2.registration.kakaoclientId="$CLIENT_ID" \
+  --spring.security.oauth2.registration.kakaoclientSecret="$CLIENT_SECRET" \
+  --spring.security.oauth2.registration.kakaoredirectUri="$REDIRECT_URI" \
+  --cloud.aws.credentials.accessKey="$ACCESS_KEY" \
+  --cloud.aws.credentials.secretKey="$SECRET_KEY" \
+  --cloud.aws.s3.bucket="$BUCKET_NAME" \
+  --cors.allowd-origins="$FRONT_URL" \
+  --app.auth.tokenSecret="$TOKEN_SECRET" \
+  --app.auth.refreshTokenSecret="$REFRESH_TOKEN_SECRET" \
+  --app.auth.tokenExpiration="$EXPIRATION_TIME" \
+  --app.auth.refreshTokenExpiration="$REFRESH_EXPIRATION_TIME" \
+  --app.cors.allowedOrigins="$FRONT_URL" \
+  --app.oauth2.authorizedRedirectUris="$FRONT_URL"
