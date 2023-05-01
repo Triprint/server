06f0d31bba94985aa064407a73aff6fd9a1e0779
dana <seonhui9759@naver.com>
2023-05-01 21:22:02 +0900

    feat: 검색 정렬 기능 구현 - QueryDslUtil생성하여 정렬 기준이 되는 칼럼의 Path와 그 칼럼이 속한 엔티티가 필요해 Expression target을 핸들링하기 위한  getSortedColumn메서드 구현 - SearchRepositoryImpl에서 정렬을 사용하기 위해 getAllOrderSpecifiers메서드를 생성

--- src/main/java/com/triprint/backend/domain/search/controller/SearchController.java
+++ src/main/java/com/triprint/backend/domain/search/controller/SearchController.java
@@ -33,8 +33,8 @@ public class SearchController {
 
 	@GetMapping("/location")
 	public ResponseEntity<Page<GetPostResponse>> searchBasedOnLocation(
-		GetLocationRequest getLocationRequest,
-		@PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable page
+		@PageableDefault(sort = "id", direction = Sort.Direction.DESC) Pageable page,
+		GetLocationRequest getLocationRequest
 	) {
 		return ResponseEntity.ok(searchService.searchBasedOnLocation(page, getLocationRequest));
 	}
--- src/main/java/com/triprint/backend/domain/search/repository/SearchRepositoryImpl.java
+++ src/main/java/com/triprint/backend/domain/search/repository/SearchRepositoryImpl.java
@@ -3,17 +3,24 @@ package com.triprint.backend.domain.search.repository;
 import static com.triprint.backend.domain.location.entity.QDistrict.*;
 import static com.triprint.backend.domain.location.entity.QTouristAttraction.*;
 import static com.triprint.backend.domain.post.entity.QPost.*;
+import static org.springframework.util.ObjectUtils.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.triprint.backend.domain.location.entity.City;
 import com.triprint.backend.domain.location.entity.District;
 import com.triprint.backend.domain.post.entity.Post;
+import com.triprint.backend.domain.post.entity.QPost;
+import com.triprint.backend.domain.search.util.QueryDslUtil;
 
 import lombok.RequiredArgsConstructor;
 
@@ -23,12 +30,56 @@ public class SearchRepositoryImpl implements SearchRepositoryCustom {
 
 	@Override
 	public Page<Post> findBySearchBasedOnCityAndDistrictKeywords(Pageable pageable, City city, District district0) {
+
+		List<OrderSpecifier> orders = getAllOrderSpecifiers(pageable);
+
 		List<Post> result = (List<Post>)jpaQueryFactory.from(post)
 			.join(post.touristAttraction, touristAttraction)
 			.join(touristAttraction.district, district)
 			.where(district.eq(district0), district.city.eq(city))
+			.orderBy(orders.stream().toArray(OrderSpecifier[]::new))
+			.offset(pageable.getOffset())
+			.limit(pageable.getPageSize())
 			.fetch();
 		return new PageImpl<>(result);
+	}
+
+	/**
+	 * Order의 칼럼이 없는 경우
+	 * 1. 값 무시 -> 메서드 만들어서
+	 * 2. 에러 터뜨리기 -> 에러가 터지면 나만의 에러로 핸들링하기
+	 * 3. 클래스에 존재하는 칼럼인지 확인하는 메서드가 있는지 확인하기
+	 *
+	 *
+	 */
+
+	private List<OrderSpecifier> getAllOrderSpecifiers(Pageable pageable) {
+
+		if (isEmpty(pageable.getSort())) {
+			return new ArrayList<>();
+		}
+
+		List<OrderSpecifier> orders = new ArrayList<>();
+		for (Sort.Order order : pageable.getSort()) {
+			Order direction = order.getDirection().isAscending() ? Order.ASC : Order.DESC;
+			String property = order.getProperty();
+			// 만약에 Post에 없는 Column이 요청이으로 들어왔을 때?
+			OrderSpecifier<?> column = QueryDslUtil.getSortedColumn(direction, QPost.post, property);
+			orders.add(column);
+			// switch (order.getProperty()) {
+			// 	case "id":
+			// 		OrderSpecifier<?> orderId = QueryDslUtil.getSortedColumn(direction, QPost.post, "id");
+			// 		orders.add(orderId);
+			// 		break;
+			// 	case "createdAt":
+			// 		OrderSpecifier<?> orderCreatedAt = QueryDslUtil.getSortedColumn(direction, QPost.post, "createdAt");
+			// 		orders.add(orderCreatedAt);
+			// 		break;
+			// 	default:
+			// 		break;
+			// }
+		}
+		return orders;
 	}
 
 	@Override
