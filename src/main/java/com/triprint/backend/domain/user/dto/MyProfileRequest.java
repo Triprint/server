f7bc0187e3cde86da96f5d23f075c5becef98f94
dana <seonhui9759@naver.com>
2023-05-23 21:43:43 +0900

    user의 DTO들에 Validation추가 완료

--- src/main/java/com/triprint/backend/domain/user/dto/MyProfileRequest.java
+++ src/main/java/com/triprint/backend/domain/user/dto/MyProfileRequest.java
@@ -1,8 +1,11 @@
 package com.triprint.backend.domain.user.dto;
 
+import org.springframework.lang.NonNull;
+
 import lombok.Getter;
 
 @Getter
 public class MyProfileRequest {
+	@NonNull
 	private String username;
 }
